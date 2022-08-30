e65b3f6fc4d8f09862327e43679cb04344e38ee7
Eric Nantes <ericnantes18@gmail.com>
2022-08-30 09:18:44 -0400

    Creating App container and Sidebar

--- 01-FUNDAMENTOS-REACTJS/src/App.jsx
+++ 01-FUNDAMENTOS-REACTJS/src/App.jsx
@@ -1,11 +1,22 @@
-import './globalStyles.css';
-import {FaThumbsUp} from 'react-icons/fa';
 import { Header } from './components/Header';
+
+import styles from './App.module.css';
+import './globalStyles.css';
+import { Sidebar } from './components/Sidebar';
 
 function App() {
   return (
     <>
-      <Header />    
+      <Header />
+
+      <div className={styles.wrapper}>
+       <Sidebar />
+
+       <main>
+          <p>Post</p>
+       </main>
+
+      </div>
     </>
   )
 }
---
+++ 01-FUNDAMENTOS-REACTJS/src/components/Sidebar/index.jsx
@@ -0,0 +1,23 @@
+import styles from './styles.module.css'
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img 
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1566577134624-6f6cc4bb272b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=40" 
+      />
+
+      <div className={styles.profile}>
+        <strong>Eric Nantes</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  )
+}
