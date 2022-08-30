f69137e794ca963f120eae86c0c05e0a80dbfacf
Eric Nantes <ericnantes18@gmail.com>
2022-08-30 09:47:34 -0400

    Finalizing styles of Sidebar

--- 01-FUNDAMENTOS-REACTJS/src/components/Sidebar/index.jsx
+++ 01-FUNDAMENTOS-REACTJS/src/components/Sidebar/index.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react';
+
 import styles from './styles.module.css'
 
 export function Sidebar() {
@@ -9,12 +11,15 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/ericoptero.png" />
+
         <strong>Eric Nantes</strong>
         <span>Web Developer</span>
       </div>
 
       <footer>
         <a href="#">
+          <PencilLine size="1.25rem" />
           Editar seu perfil
         </a>
       </footer>
