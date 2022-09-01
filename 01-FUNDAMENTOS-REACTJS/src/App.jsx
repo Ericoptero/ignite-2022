56a98b431f29df88fbcb61f4272d7995bff56931
Eric Nantes <ericnantes18@gmail.com>
2022-09-01 16:47:54 -0400

    Mapping content for posts

--- 01-FUNDAMENTOS-REACTJS/src/App.jsx
+++ 01-FUNDAMENTOS-REACTJS/src/App.jsx
@@ -5,6 +5,39 @@ import { Post } from './components/Post';
 import styles from './App.module.css';
 import './globalStyles.css';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/ericoptero.png',
+      name: 'Eric Nantes',
+      role: 'Web Developer',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+      { type: 'hashtag', content: ['#novoprojeto', '#nlw', '#rocketseat'] }
+    ],
+    publishedAt: new Date('2022-08-31 20:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/octocat.png',
+      name: 'The Octocat',
+      role: 'A github cat',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+      { type: 'hashtag', content: ['#novoprojeto', '#nlw', '#rocketseat'] }
+    ],
+    publishedAt: new Date('2022-09-01 02:00:00')
+  },
+];
+
 function App() {
   return (
     <>
@@ -14,8 +47,14 @@ function App() {
        <Sidebar />
 
        <main>
-          <Post />
-          <Post />
+          {posts.map(post => (
+            <Post 
+              key={post.id}
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />
+          ))}
        </main>
 
       </div>
