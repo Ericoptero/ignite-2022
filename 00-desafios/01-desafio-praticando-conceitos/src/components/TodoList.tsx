341d7d438fca5556fcef258280f9c68a2c56f75a
Eric Nantes <ericnantes18@gmail.com>
2022-09-01 22:50:02 -0400

    01-challenge/Styling Task list

--- 00-desafios/01-desafio-praticando-conceitos/src/components/TodoList.tsx
+++ 00-desafios/01-desafio-praticando-conceitos/src/components/TodoList.tsx
@@ -1,5 +1,8 @@
 import styles from './TodoList.module.css';
 
+import clipboardImg from '../img/clipboard.svg'
+import { CheckCircle, Circle, Trash } from 'phosphor-react';
+
 export function TodoList() {
   return (
     <>
@@ -14,11 +17,45 @@ export function TodoList() {
         </p>
       </header>
       
+      <section className={styles.emptyTasks}>
+        <img src={clipboardImg} />
+        <strong>Você ainda não tem tarefas cadastradas</strong>
+        <p>Crie tarefas e organize seus itens a fazer</p>
+      </section>
+
       <section>
-        <div>
-          <img src="" alt="" />
-          <strong>Você ainda não tem tarefas cadastradas</strong>
-          <p>Crie tarefas e organize seus itens a fazer</p>
+        <div className={styles.task}>
+          {/* <CheckCircle className={styles.checkCircle} size="1.125rem" weight='fill'/> */}
+          <Circle className={styles.circle} size="1.125rem" />
+          <p>
+            Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.
+          </p>
+          <button>
+            <Trash size=".875rem" />
+          </button>
+        </div>
+
+
+        <div className={`${styles.task} ${styles.taskChecked}`}>
+          <CheckCircle className={styles.checkCircle} size="1.125rem" weight='fill'/>
+          {/* <Circle className={styles.circle} size="1.125rem" /> */}
+          <p>
+            Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.
+          </p>
+          <button>
+            <Trash size=".875rem" />
+          </button>
+        </div>
+
+        <div className={styles.task}>
+          {/* <CheckCircle className={styles.checkCircle} size="1.125rem" weight='fill'/> */}
+          <Circle className={styles.circle} size="1.125rem" />
+          <p>
+            Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.
+          </p>
+          <button>
+            <Trash size=".875rem" />
+          </button>
         </div>
       </section>
     </>
